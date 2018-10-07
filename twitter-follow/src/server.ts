d486efb2beb709ebe5a8a5714b2669c058e0f403
Michael Leontieff <michael@leontieff.net>
2018-10-07 13:51:00 +1000

    setup initial server code

--- twitter-follow/src/server.ts
+++ twitter-follow/src/server.ts
@@ -1,11 +1,55 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
+import * as logger from "morgan";
 
 const app = express();
 app.use(express.static('serve/client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(8000, () => {
-    console.log('server started');
-});
+dotenv.config({ path: ".env.configuration" });
+
+class App {
+
+    // ref to Express instance
+    public express: express.Application;
+  
+    constructor() {
+      this.express = express();
+      this.middleware();
+      this.routes();
+      this.launchConf();
+  
+    }
+    private middleware(): void {
+      this.express.set("port", process.env.PORT || 3000);
+      
+      this.express.use(logger("dev"));
+      this.express.use(bodyParser.json());
+      this.express.use(bodyParser.urlencoded({ extended: true }));
+    }
+    
+    /**
+     * Primary app routes.
+     */
+    private routes(): void {
+      //this.express.use("/", rootRouter);
+      //this.express.use("/api", apiRouter);
+    }
+  
+    private launchConf() {
+  
+      /**
+       * Start Express server.
+       */
+      this.express.listen(this.express.get("port"), () => {
+        console.log(("  App is running at http://localhost:%d \
+        in %s mode"), this.express.get("port"), this.express.get("env"));
+        console.log("  Press CTRL-C to stop\n");
+      });
+    }
+  }
+  
+  export default new App().express;
+  
