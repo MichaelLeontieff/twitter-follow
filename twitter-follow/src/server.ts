69f5a253b71809eefc76174846e76efadf4aca0f
Michael Leontieff <michael@leontieff.net>
2018-10-21 10:14:02 +1000

    removed demo code from server.ts

--- twitter-follow/src/server.ts
+++ twitter-follow/src/server.ts
@@ -48,11 +48,6 @@ class App {
     }
   
     private launchConf() {
-      // this.oauthTest();
-      // this.twitterStreamTest();
-      // this.twitterAPITest();
-      // this.sentimentAnalysisTest();
-      // this.redisTest();
   
       /**
        * Start Express server.
@@ -63,98 +58,6 @@ class App {
         console.log("  Press CTRL-C to stop\n");
       });
     }
-
-  private oauthTest() {
-    this.express.get('/twitter/authenticate', (req, res) => {
-      let encode_secret = new Buffer(process.env.twitter_consumer_key + ':' + process.env.twitter_consumer_secret).toString('base64');
-
-      let options = {
-        url: 'https://api.twitter.com/oauth2/token',
-        headers: {
-            'Authorization': 'Basic ' + encode_secret,
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-        body: 'grant_type=client_credentials'
-      };
-
-      request.post(options, (err, response, body) => {
-        if (err) {
-            throw new Error("Failed to fetch bearer token for twitter");
-        } else {
-            process.env.twitter_bearer_token = JSON.parse(body).access_token;
-            res.json(body);
-        } 
-      });
-
-    });
-  }
-
-  private twitterStreamTest() {
-    this.express.get('/twitter/stream', (req, res) => {
-
-        let client = new Twitter({
-            consumer_key: process.env.twitter_consumer_key,
-            consumer_secret: process.env.twitter_consumer_secret,
-            access_token_key: process.env.twitter_access_token_key,
-            access_token_secret: process.env.twitter_access_token_secret
-            //bearer_token: process.env.twitter_bearer_token
-        });
-
-        let stream = client.stream('statuses/filter', {track: 'javascript'});
-        let record;
-
-        stream.on('data', (event) => {
-          record = event;
-          record && res.json(record);
-          stream.destroy();
-        });
-
-        stream.on('error', (error) => {
-          record = error;
-          record && res.json(record);
-        });
-
-        
-    });
-  }
-
-  private twitterAPITest() {
-    this.express.get('/twitter/api', (req, res) => {
-
-        let client = new Twitter({
-            consumer_key: process.env.twitter_consumer_key,
-            consumer_secret: process.env.twitter_consumer_secret,
-            bearer_token: process.env.twitter_bearer_token
-        });
-
-        var req = client.get('search/tweets', {q: 'javascript'}, (error, tweets, response) => {
-            console.log(error, tweets, response);
-            res.json(tweets);
-        });
-    });
-  }
-
-  private sentimentAnalysisTest() {
-    let Analyzer = natural.SentimentAnalyzer;
-    let stemmer = natural.PorterStemmer;
-    let analyzer = new Analyzer("English", stemmer, "afinn")
-    console.log(analyzer.getSentiment(["I", "hate", "cherries"]));
-  }
-
-  private redisTest() {
-    // this.express.get('/redis', (req, res) => {
-
-    //   client.on('error', (err) => {
-    //     console.log("Something went wrong", err);
-    //   });
-
-    //   client.set('my test key', 'my test value', redis.print);
-    //   client.get('my test key', (err, result) => {
-    //     if (err) throw err;
-    //     res.json(result);
-    //     console.log('GET result ->', result);
-    //   });
-    // });
-  }
 }
   
 export default new App().express;
